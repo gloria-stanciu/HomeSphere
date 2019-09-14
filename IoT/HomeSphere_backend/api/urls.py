91d78b1df1d2632be104262aa3c96e11b1c7f437
Arpad Gabor <arpig.razzey@gmail.com>
2019-09-14 17:28:50 +0300

    probably does not work

--- IoT/HomeSphere_backend/api/urls.py
+++ IoT/HomeSphere_backend/api/urls.py
@@ -1,11 +1,16 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from . import views
 
-router = routers.DefaultRouter()
-router.register(r'sensordata', views.SensorDataViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'sensordatas', views.SensorDataViewSet.as_view())
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('sensordata/', views.SensorDataViewSet.as_view()),
     path('auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- IoT/HomeSphere_backend/api/views.py
+++ IoT/HomeSphere_backend/api/views.py
@@ -1,7 +1,14 @@
-from rest_framework import viewsets
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import action
 from .serializers import SensorDataSerializer
 from .models import SensorData
 
-class SensorDataViewSet(viewsets.ModelViewSet):
-    queryset = SensorData.objects.all().order_by('deviceID')
-    serializer_class = SensorDataSerializer
+class SensorDataViewSet(APIView):
+    def get(self, request, pk=None):
+        queryset = SensorData.objects.all().order_by('deviceID')
+        serializer_class = SensorDataSerializer(queryset, many=True)
+        return Response(queryset)
